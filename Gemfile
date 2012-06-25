d66495157586eb06c8ede28cfe28057943ab9b5c
Winfred <winfred.nadeau@gmail.com>
2012-06-25 10:58:34 -0400

    initial commit - adds devise user model

---
+++ Gemfile
@@ -0,0 +1,25 @@
+source 'https://rubygems.org'
+
+gem 'rails', '3.2.6'
+
+gem 'sqlite3'
+gem 'devise'
+gem 'heroku'
+
+group :assets do
+  gem 'sass-rails',   '~> 3.2.3'
+  gem 'coffee-rails', '~> 3.2.1'
+  gem 'uglifier', '>= 1.0.3'
+  gem 'less-rails'
+  gem 'twitter-bootstrap-rails'
+end
+
+gem 'jquery-rails'
+
+group :test, :development do
+  gem 'rspec-rails'
+  gem 'factory_girl_rails', '1.2.0'
+  gem 'powder'
+end
+
+
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Rhok::Application.config.secret_token = 'e6798d39de140620010628bee0f9c6346225a5846c4db06a6c5d2e7b2cabe555cf288ae971304d3ddbd49987707685dc1fd9d223ca96d2ab653cd417233fd3c0'
