6c6214a2cbe2219107c494fe25f0f74a9c30efdf
Winfred <winfred.nadeau@gmail.com>
2012-07-05 15:50:29 -0400

    adding standard sessions_controller

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,26 @@
+class SessionsController < Devise::SessionsController
+
+  # GET /users/sign_in
+  def new
+    user = User.new
+    clean_up_passwords(user)
+    respond_with(user, serialize_options(user))
+  end
+
+  # POST /users/sign_in
+  def create
+    user = warden.authenticate!(:scope => "user", :recall => "sessions#new")
+    set_flash_message(:notice, :signed_in) if is_navigational_format?
+    sign_in("user", user)
+    respond_with user, :location => after_sign_in_path_for(user)
+  end
+
+  # DELETE /users/sign_out
+  def destroy
+    redirect_path = after_sign_out_path_for("user")
+    signed_out = (Devise.sign_out_all_scopes ? sign_out : sign_out("user"))
+    set_flash_message :notice, :signed_out if signed_out && is_navigational_format?
+    super
+  end
+
+end
