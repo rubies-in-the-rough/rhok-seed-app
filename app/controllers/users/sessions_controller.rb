9f3cb5daff7d28a3f8a670a5ae5ecd4ed209032b
Winfred <winfred.nadeau@gmail.com>
2012-07-12 00:31:58 -0400

    sessions#create passes the scope to warden as a symbol

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -8,7 +8,7 @@ class Users::SessionsController < Devise::SessionsController
 
   # POST /users/sign_in
   def create
-    user = warden.authenticate!(:scope => "user", :recall => "users/sessions#new")
+    user = warden.authenticate!(scope: :user, recall: "users/sessions#new")
     set_flash_message(:notice, :signed_in) if is_navigational_format?
     sign_in("user", user)
     respond_with user, :location => after_sign_in_path_for(user)
