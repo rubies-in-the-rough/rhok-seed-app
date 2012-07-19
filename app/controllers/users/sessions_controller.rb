e6a50ab0ab466504aef8aad16de7ff5ee015b858
Winfred <winfred.nadeau@gmail.com>
2012-07-19 15:36:56 -0400

    adding comments to session controller

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -1,12 +1,14 @@
 class Users::SessionsController < Devise::SessionsController
 
   # GET /users/sign_in
+  # Render the sign in form
   def new
     user = User.new
     respond_with(user, serialize_options(user))
   end
 
   # POST /users/sign_in
+  # Authenticate a user and create a new user session if valid
   def create
     user = warden.authenticate!(scope: :user, recall: "users/sessions#new")
     set_flash_message(:notice, :signed_in) if is_navigational_format?
@@ -15,6 +17,7 @@ class Users::SessionsController < Devise::SessionsController
   end
 
   # DELETE /users/sign_out
+  # End a user session
   def destroy
     redirect_path = after_sign_out_path_for("user")
     signed_out = (Devise.sign_out_all_scopes ? sign_out : sign_out("user"))
