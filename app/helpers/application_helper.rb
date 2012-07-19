e8a3038b226f81eeb7c12cc22780b6619e6f110e
Winfred <winfred.nadeau@gmail.com>
2012-07-19 15:32:23 -0400

    adding comments to helper methods

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,8 @@
 module ApplicationHelper
+  #
+  # Provide the appropriate class identifier for the page
+  #  This helps mark the active page as active in the nav menu
+  #
   def current_page_class(page)
     return "class='active'" if current_page?(page)
     return ""
