d2146010d711bf7b4fd63ac5bb37ff2764ad89be
Winfred <winfred.nadeau@gmail.com>
2012-07-26 15:01:02 -0400

    get rspec running

---
+++ spec/controllers/admin/seeds_controller_spec.rb
@@ -0,0 +1,5 @@
+require 'spec_helper'
+
+describe Admin::SeedsController do
+
+end
--- spec/controllers/seeds_controller_spec.rb
+++ spec/controllers/seeds_controller_spec.rb
@@ -1,6 +1,6 @@
 require 'spec_helper'
 
-describe SeedController do
+describe SeedsController do
 
   describe "GET 'index'" do
     it "returns http success" do
